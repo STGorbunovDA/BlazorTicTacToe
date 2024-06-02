5d42676f302a092faa6488e8fc247d5be3042476
Дмитрий Горбунов <bous07@mail.ru>
2024-06-02 17:13:59 +0300

    Add Game Real-Time App with Blazor

---
+++ src/BlazorTicTacToe/BlazorTicTacToe/Hubs/GameHub.cs
@@ -0,0 +1,79 @@
+﻿using BlazorTicTacToe.Shared;
+using Microsoft.AspNetCore.SignalR;
+
+namespace BlazorTicTacToe.Hubs
+{
+    public class GameHub : Hub
+    {
+        private static readonly List<GameRoom> _rooms = [];
+
+        public override async Task OnConnectedAsync()
+        {
+            Console.WriteLine($"Player with Id '{Context.ConnectionId}' conndected.");
+
+            await Clients.Caller.SendAsync("Rooms", _rooms.OrderBy(r => r.RoomName));
+        }
+
+        public async Task<GameRoom> CreateRoom(string nameRoom, string playerName)
+        {
+            var roomId = Guid.NewGuid().ToString();
+            var room = new GameRoom(roomId, nameRoom);
+            _rooms.Add(room);
+
+            var newPlayer = new Player(Context.ConnectionId, playerName);
+            room.TryAddPlayer(newPlayer);
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+
+            await Clients.All.SendAsync("Rooms", _rooms.OrderBy(r => r.RoomName));
+
+            return room;
+        }
+
+        public async Task<GameRoom?> JoinRoom(string roomId, string playerName)
+        {
+            var room = _rooms.FirstOrDefault(r => r.RoomId == roomId);
+            if (room is not null)
+            {
+                var newPlayer = new Player(Context.ConnectionId, playerName);
+                if (room.TryAddPlayer(newPlayer))
+                {
+                    await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+                    await Clients.Group(roomId).SendAsync("PlayerJoined", newPlayer);
+                    return room;
+                }
+            }
+
+            return null;
+        }
+
+        public async Task StartGame(string roomId)
+        {
+            var room = _rooms.FirstOrDefault(r => r.RoomId == roomId);
+
+            if (room is not null)
+            {
+                room.Game.StartGame();
+                await Clients.Group(roomId).SendAsync("UpdateGame", room);
+            }
+        }
+
+        public async Task MakeMove(string roomId, int row, int col, string playerId)
+        {
+            var room = _rooms.FirstOrDefault(r => r.RoomId == roomId);
+
+            if (room is not null && room.Game.MakeMove(row, col, playerId))
+            {
+                room.Game.Winner = room.Game.CheckWinner();
+                room.Game.IsDraw = room.Game.CheckDraw() && string.IsNullOrEmpty(room.Game.Winner);
+
+                if (!string.IsNullOrEmpty(room.Game.Winner) || room.Game.IsDraw)
+                {
+                    room.Game.GameOver = true;
+                }
+
+                await Clients.Group(roomId).SendAsync("UpdateGame", room);
+            }
+        }
+    }
+}
